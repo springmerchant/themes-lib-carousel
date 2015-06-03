0f80573c024221f39d50a1a4f6708bfaa29b0598
essmahr <essmahr@gmail.com>
2015-06-03 12:38:37 -0700

    add imagesloaded dep

--- config.js
+++ config.js
@@ -19,6 +19,7 @@ System.config({
     "babel": "npm:babel-core@5.4.7",
     "babel-runtime": "npm:babel-runtime@5.4.7",
     "core-js": "npm:core-js@0.9.13",
+    "imagesloaded": "npm:imagesloaded@3.1.8",
     "jquery": "github:components/jquery@2.1.4",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
@@ -27,6 +28,13 @@ System.config({
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:imagesloaded@3.1.8": {
+      "eventie": "npm:eventie@1.0.6",
+      "wolfy87-eventemitter": "npm:wolfy87-eventemitter@4.2.11"
+    },
+    "npm:wolfy87-eventemitter@4.2.11": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2"
     }
   }
 });
