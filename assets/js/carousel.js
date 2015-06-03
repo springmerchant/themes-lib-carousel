0c0bbfc042a79ec190d9c2d2fc13628a78b0c959
essmahr <essmahr@gmail.com>
2015-06-03 13:20:53 -0700

    fix indentation

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -4,96 +4,96 @@ import trend from 'jquery-trend';
 
 export default class Carousel {
 
-    constructor(options) {
-        super();
-        this.$el = $(options.el);
+  constructor(options) {
+    super();
+    this.$el = $(options.el);
 
-        this.options = $.extend({
-            carouselDelay: 4000
-        }, options);
+    this.options = $.extend({
+      carouselDelay: 4000
+    }, options);
 
-        this.$item = this.$el.find('.carousel-item');
+    this.$item = this.$el.find('.carousel-item');
 
-        this.bindEvents();
-        this.setCarouselHeight();
-        this.startLoop();
+    this.bindEvents();
+    this.setCarouselHeight();
+    this.startLoop();
+  }
+
+  bindEvents() {
+    this.$el.on('click', this.$el.find('.carousel-navigation-item'), (e) => {
+      if ($(e.target).hasClass('next')) {
+        this.nextSlide();
+      } else {
+        this.previousSlide();
+      }
+    });
+
+    this.$el.on('mouseenter', () => {
+      this.pauseLoop();
+    });
+
+    this.$el.on('mouseleave', () => {
+      this.startLoop();
+    });
+
+    $(window).on('resize', () => {
+      this.setCarouselHeight();
+    });
+  }
+
+  setCarouselHeight() {
+    this.$el.imagesLoaded(() => {
+      let carouselHeight = 0;
+
+      for (let i of this.$item.length) {
+        let $itemHeight = this.$item.eq(i).height();
+
+        carouselHeight = Math.max(carouselHeight, $itemHeight);
+      }
+
+      this.$el.height(carouselHeight);
+    });
+  }
+
+  previousSlide() {
+    this.showNewSlide('prev');
+  }
+
+  nextSlide() {
+    this.showNewSlide('next');
+  }
+
+  showNewSlide(type) {
+    let direction = (type === 'next') ? 'left' : 'right';
+    let fallback = (type === 'next') ? 'first' : 'last';
+
+    let activeSlide = $('.carousel-item.active');
+    let nextSlide = (activeSlide[type]().hasClass('carousel-item')) ? activeSlide[type]() : this.$item[fallback]();
+
+    if (!nextSlide.length) {
+      nextSlide = this.$item[fallback]();
     }
 
-    bindEvents() {
-        this.$el.on('click', this.$el.find('.carousel-navigation-item'), (e) => {
-            if ($(e.target).hasClass('next')) {
-                this.nextSlide();
-            } else {
-                this.previousSlide();
-            }
-        });
+    nextSlide.addClass(type);
+    nextSlide[0].offsetWidth; // force reflow
+    activeSlide.addClass(direction);
+    nextSlide.addClass(direction);
 
-        this.$el.on('mouseenter', () => {
-            this.pauseLoop();
-        });
+    nextSlide.one('trend', function() {
+      nextSlide.removeClass([type, direction].join(' ')).addClass('active');
+      activeSlide.removeClass(['active', direction].join(' '));
+    });
+  }
 
-        this.$el.on('mouseleave', () => {
-            this.startLoop();
-        });
+  startLoop() {
+    let delay = this.options.carouselDelay * 1000;
 
-        $(window).on('resize', () => {
-            this.setCarouselHeight();
-        });
-    }
+    this.autoplay = setInterval(() => {
+      this.nextSlide();
+    }, delay);
+  }
 
-    setCarouselHeight() {
-        this.$el.imagesLoaded(() => {
-            let carouselHeight = 0;
-
-            for (let i of this.$item.length) {
-                let $itemHeight = this.$item.eq(i).height();
-
-                carouselHeight = Math.max(carouselHeight, $itemHeight);
-            }
-
-            this.$el.height(carouselHeight);
-        });
-    }
-
-    previousSlide() {
-        this.showNewSlide('prev');
-    }
-
-    nextSlide() {
-        this.showNewSlide('next');
-    }
-
-    showNewSlide(type) {
-        let direction = (type === 'next') ? 'left' : 'right';
-        let fallback = (type === 'next') ? 'first' : 'last';
-
-        let activeSlide = $('.carousel-item.active');
-        let nextSlide = (activeSlide[type]().hasClass('carousel-item')) ? activeSlide[type]() : this.$item[fallback]();
-
-        if (!nextSlide.length) {
-            nextSlide = this.$item[fallback]();
-        }
-
-        nextSlide.addClass(type);
-        nextSlide[0].offsetWidth; // force reflow
-        activeSlide.addClass(direction);
-        nextSlide.addClass(direction);
-
-        nextSlide.one('trend', function() {
-            nextSlide.removeClass([type, direction].join(' ')).addClass('active');
-            activeSlide.removeClass(['active', direction].join(' '));
-        });
-    }
-
-    startLoop() {
-        let delay = this.options.carouselDelay * 1000;
-
-        this.autoplay = setInterval(() => {
-            this.nextSlide();
-        }, delay);
-    }
-
-    pauseLoop() {
-        clearInterval(this.autoplay);
-    }
+  pauseLoop() {
+    clearInterval(this.autoplay);
+  }
 }
