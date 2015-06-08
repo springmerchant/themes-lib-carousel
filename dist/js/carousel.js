3059d7da8d3894ac98792bba64a5310dc00efb08
essmahr <essmahr@gmail.com>
2015-06-08 12:40:57 -0700

    carousel now works off index var

--- dist/js/carousel.js
+++ dist/js/carousel.js
@@ -7,11 +7,13 @@ export default class Carousel {
     super();
     this.$el = $(options.el);
 
+    this.currentIndex = 0;
+
     this.options = $.extend({
       delay: 4000
     }, options);
 
-    this.$item = this.$el.find('.carousel-item');
+    this.$items = this.$el.find('.carousel-item');
 
     this.bindEvents();
     this.setCarouselHeight();
@@ -44,8 +46,8 @@ export default class Carousel {
     this.$el.imagesLoaded(() => {
       let carouselHeight = 0;
 
-      for (let i of this.$item.length) {
-        let itemHeight = this.$item.eq(i).height();
+      for (let i of this.$items.length) {
+        let itemHeight = this.$items.eq(i).height();
 
         carouselHeight = Math.max(carouselHeight, itemHeight);
       }
@@ -55,33 +57,37 @@ export default class Carousel {
   }
 
   previousSlide() {
-    this.showNewSlide('prev');
+    // to the end if we're on the first slide, otherwise decrement
+    let newIndex = (this.currentIndex === 0) ? this.$items.length - 1 : this.currentIndex - 1;
+
+    this.showNewSlide(newIndex);
   }
 
   nextSlide() {
-    this.showNewSlide('next');
+    // back to zero if we're on the last slide, otherwise increment
+    let newIndex = (this.currentIndex + 1 === this.$items.length) ? 0 : this.currentIndex + 1;
+
+    this.showNewSlide(newIndex);
   }
 
-  showNewSlide(type) {
-    let direction = (type === 'next') ? 'left' : 'right';
-    let fallback = (type === 'next') ? 'first' : 'last';
+  showNewSlide(newIndex) {
 
-    let activeSlide = $('.carousel-item.active');
-    let nextSlide = (activeSlide[type]().hasClass('carousel-item')) ? activeSlide[type]() : this.$item[fallback]();
+    // determine if we're going left or right
+    let direction = (newIndex > this.currentIndex) ? 'left' : 'right';
 
-    if (!nextSlide.length) {
-      nextSlide = this.$item[fallback]();
-    }
+    let $activeSlide = $('.carousel-item.active');
+    let $nextSlide = $(this.$items[newIndex]);
 
-    nextSlide.addClass(type);
-    nextSlide[0].offsetWidth; // force reflow
-    activeSlide.addClass(direction);
-    nextSlide.addClass(direction);
+    $nextSlide[0].offsetWidth; // force reflow
+    $activeSlide.addClass(direction);
+    $nextSlide.addClass(direction);
 
-    nextSlide.one('trend', function() {
-      nextSlide.removeClass(`${type} ${direction}`).addClass('active');
-      activeSlide.removeClass(`active ${direction}`);
+    $nextSlide.one('trend', function() {
+      $nextSlide.removeClass(direction).addClass('active');
+      $activeSlide.removeClass(`active ${direction}`);
     });
+
+    this.currentIndex = newIndex;
   }
 
   startLoop() {
