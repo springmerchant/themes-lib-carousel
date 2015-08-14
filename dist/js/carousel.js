dc8795248ab1929be68cd24f6a58d4d14baff3f9
Joseph North <joseph@pixelunion.net>
2015-08-14 13:18:49 -0700

    Change slides using a target index to better allow rapid changes.
    Calling changeSlide now sets a targetIndex that the carousel should eventually
    show. If the carousel is currently idle, it will be shown right away. If the
    carousel is currently animating another slide change, the target will be shown
    after the current animation.

--- dist/js/carousel.js
+++ dist/js/carousel.js
@@ -8,7 +8,10 @@ export default class Carousel {
     this.$el = $(options.el);
 
     this.currentIndex = 0;
+    this.targetIndex = 0;
     this.isPlaying = false;
+    this.isChanging = false;
+    this.animationDirection;
 
     this.options = $.extend({
       delay: 4000,
@@ -31,6 +34,9 @@ export default class Carousel {
     this.$items.first().revealer('show', true);
   }
 
+  /**
+   * Start slideshow.
+   */
   play() {
     if (this.isPlaying) {
       return;
@@ -40,6 +46,9 @@ export default class Carousel {
     this._startLoop();
   }
 
+  /**
+   * Pause slideshow.
+   */
   pause() {
     if (!this.isPlaying) {
       return;
@@ -49,6 +58,61 @@ export default class Carousel {
     this._pauseLoop();
   }
 
+  /**
+   * Select the previous slide.
+   */
+  previousSlide() {
+    const newIndex = (this.targetIndex === 0)
+      ? this.$items.length - 1
+      : this.targetIndex - 1;
+    this.changeSlide(newIndex, 'right');
+  }
+
+  /**
+   * Select the next slide.
+   */
+  nextSlide() {
+    const newIndex = (this.targetIndex + 1 === this.$items.length)
+      ? 0
+      : this.targetIndex + 1;
+    this.changeSlide(newIndex, 'left');
+  }
+
+  /**
+   * Select a specific slide.
+   *
+   * @param {Number} targetIndex
+   *        The index of the slide to select.
+   *
+   * @param {String} animationDirection = 'auto'
+   *        A class name that will be applied to the slide while animating. This
+   *        allows you to animate specific slides differently.
+   *        The value will be prepended with `animating-`.
+   *        Defaults to `left` or `right`.
+   */
+  changeSlide(targetIndex, animationDirection = 'auto') {
+    // Calculate animation direction
+    let direction = null;
+
+    if (animationDirection === 'auto') {
+      direction = (targetIndex > this.currentIndex)
+        ? 'animating-left'
+        : 'animating-right';
+    } else {
+      direction = `animating-${animationDirection}`;
+    }
+
+    // Update state
+    this._updatePagination(targetIndex);
+    this.targetIndex = targetIndex;
+    this.animationDirection = direction;
+    this._updateSlideState();
+  }
+
+  /**
+   * @private
+   * Set up event handlers.
+   */
   _bindEvents() {
     this.$el.on('click', '.carousel-navigation-item', (e) => {
       if ($(e.target).hasClass('carousel-next')) {
@@ -75,6 +139,10 @@ export default class Carousel {
     });
   }
 
+  /**
+   * @private
+   * Update the carousel wrapper height to match the largest slide.
+   */
   _setCarouselHeight() {
     this.$el.trigger('carousel-resize');
 
@@ -91,10 +159,18 @@ export default class Carousel {
     });
   }
 
+  /**
+   * @private
+   * Show navigation element.
+   */
   _displayNavigation() {
     this.$el.find('.carousel-navigation').addClass('visible');
   }
 
+  /**
+   * @private
+   * Create pagination elements.
+   */
   _initPagination() {
     // skip if it's not activated
     if (!this.options.pagination) {
@@ -118,30 +194,39 @@ export default class Carousel {
     this._updatePagination(this.currentIndex);
   }
 
+  /**
+   * @private
+   * Update pagination elements to match selected slide.
+   */
   _updatePagination(index) {
-    this.$el.find('.carousel-pagination-item').removeClass('active').filter(`[data-slide=${index}]`).addClass('active');
+    this.$el
+      .find('.carousel-pagination-item')
+      .removeClass('active')
+      .filter(`[data-slide=${index}]`)
+      .addClass('active');
   }
 
-  previousSlide() {
-    // to the end if we're on the first slide, otherwise decrement
-    const newIndex = (this.currentIndex === 0) ? this.$items.length - 1 : this.currentIndex - 1;
-
-    this.changeSlide(newIndex, 'right');
-  }
-
-  nextSlide() {
-    // back to zero if we're on the last slide, otherwise increment
-    const newIndex = (this.currentIndex + 1 === this.$items.length) ? 0 : this.currentIndex + 1;
-
-    this.changeSlide(newIndex, 'left');
-  }
-
-  changeSlide(targetIndex, animationDirection = 'auto') {
-    const currentIndex = this.currentIndex;
-
-    if (currentIndex === targetIndex) {
+  /**
+   * @private
+   * Select the slide currently set as the target.
+   */
+  _updateSlideState() {
+    // Skip if we are already at our target slide
+    if (this.currentIndex === this.targetIndex) {
       return;
     }
+
+    // Skip this if currently animating. This function calls itself after
+    // animating to handle this case.
+    if (this.isChanging) {
+      return;
+    }
+
+    this.isChanging = true;
+
+    // Save current and target index so we can pass them to our changed event
+    const currentIndex = this.currentIndex;
+    const targetIndex = this.targetIndex;
 
     // Trigger change event
     this.$el.trigger('carousel-change', {
@@ -149,47 +234,61 @@ export default class Carousel {
       to: targetIndex,
     });
 
-    // Calculate animation direction
-    let direction = null;
-
-    if (animationDirection === 'auto') {
-      direction = (targetIndex > currentIndex)
-        ? 'animating-left'
-        : 'animating-right';
-    } else {
-      direction = `animating-${animationDirection}`;
-    }
-
-    // Change slides
+    // Hide current slide
     this.$items.eq(currentIndex)
       .one('revealer-animating', (e) => {
-        $(e.currentTarget).addClass(direction);
+        $(e.currentTarget).addClass(this.animationDirection);
       })
       .one('revealer-hide', (e) => {
-        $(e.currentTarget).removeClass(direction);
+        $(e.currentTarget).removeClass(this.animationDirection);
       })
       .revealer('hide');
 
+    // Show target slide
     this.$items.eq(targetIndex)
       .one('revealer-animating', (e) => {
-        $(e.currentTarget).addClass(direction);
+        $(e.currentTarget).addClass(this.animationDirection);
       })
       .one('revealer-show', (e) => {
-        $(e.currentTarget).removeClass(direction);
+        $(e.currentTarget).removeClass(this.animationDirection);
 
         // Trigger changed event
         this.$el.trigger('carousel-changed', {
           from: currentIndex,
           to: targetIndex,
         });
+
+        // Update state
+        this.currentIndex = targetIndex;
+        this.isChanging = false;
+        this._clearAnimationClasses();
+        this._updateSlideState();
       })
       .revealer('show');
-
-    // Update state
-    this._updatePagination(targetIndex);
-    this.currentIndex = targetIndex;
   }
 
+  /**
+   * @private
+   * Removes all classes starting with `animating`.
+   *
+   * This is needed to clean up after when the user rapidly changes slides.
+   */
+  _clearAnimationClasses() {
+    for (let i of this.$items.length) {
+      const el = this.$items[i];
+
+      const classes = el.className.split(' ').filter((c) => {
+        return c.lastIndexOf('animating', 0) !== 0;
+      });
+
+      el.className = $.trim(classes.join(' '));
+    }
+  }
+
+  /**
+   * @private
+   * Start slideshow timer.
+   */
   _startLoop() {
     const delay = this.options.delay;
 
@@ -200,6 +299,10 @@ export default class Carousel {
     this.$el.trigger('carousel-play');
   }
 
+  /**
+   * @private
+   * Stop slideshow timer.
+   */
   _pauseLoop() {
     clearInterval(this.autoplay);
     this.$el.trigger('carousel-pause');
