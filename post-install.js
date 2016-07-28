449cb1fc18cb2d869e1c7675a18ed5b349ddce9f
Niall Duncan <niall@pixelunion.net>
2016-07-28 11:37:11 -0400

    Missing semi-colon

--- post-install.js
+++ post-install.js
@@ -5,7 +5,7 @@ var packageJSON = JSON.parse(fs.readFileSync('./package.json'));
 var themeDirectory = packageJSON._where;
 var moduleFolder = "../../node_modules/bc-carousel/dist/scss/carousel"
 var themeFile = themeDirectory + "/assets/scss/theme.scss";
-var data = "\r\n// bc-carousel\r\n@import \"" + moduleFolder + "\"\r\n"
+var data = "\r\n// bc-carousel\r\n@import \"" + moduleFolder + "\";\r\n"
 
 fs.appendFile(themeFile, data, (err) => {
   if (err) throw err;
