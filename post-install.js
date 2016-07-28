df8b2c376cbadfb5421f4e390f64664c834a153e
Niall Duncan <niall@pixelunion.net>
2016-07-28 11:33:35 -0400

    More cleanup

--- post-install.js
+++ post-install.js
@@ -3,7 +3,7 @@
 var fs = require("fs");
 var packageJSON = JSON.parse(fs.readFileSync('./package.json'));
 var themeDirectory = packageJSON._where;
-var moduleFolder = "/node_modules" + packageJSON._location + "/dist/scss/carousel.scss"
+var moduleFolder = "../../node_modules/bc-carousel/dist/scss/carousel"
 var themeFile = themeDirectory + "/assets/scss/theme.scss";
 var data = "// bc-carousel\r\n@import \"" + moduleFolder + "\"\r\n"
 
