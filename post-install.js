d77d92cbf56556535c4442616128b10a7c12f4cc
Niall Duncan <niall@pixelunion.net>
2016-07-28 11:30:21 -0400

    Cleanup post-install script some more

--- post-install.js
+++ post-install.js
@@ -2,18 +2,11 @@
 
 var fs = require("fs");
 var packageJSON = JSON.parse(fs.readFileSync('./package.json'));
-
 var themeDirectory = packageJSON._where;
-var assetsFolder = themeDirectory + "/assets/scss/modules/";
-var moduleFolder = themeDirectory + "/node_modules/" + packageJSON._location + "/dist/scss/carousel.scss"
-
-// gulp.src(moduleFolder)
-//   .pipe(gulp.dest(assetsFolder));
-
+var moduleFolder = "/node_modules" + packageJSON._location + "/dist/scss/carousel.scss"
 var themeFile = themeDirectory + "/assets/scss/theme.scss";
-var data = "\r\n @import \"" + moduleFolder + "\""
+var data = "// bc-carousel\r\n@import \"" + moduleFolder + "\"\r\n"
 
 fs.appendFile(themeFile, data, (err) => {
-    if (err) throw err;
-    console.log('Unable to append import to theme.scss');
-  });
+  if (err) throw err;
+});
